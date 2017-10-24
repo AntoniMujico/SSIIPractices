b9bedc5668893384f2b3984d22e44784c7158727
Korbinian27_2 <Korbinian27_2@bitbucket.org>
2017-10-24 19:00:47 +0200

    added monthly report

--- src/IntegrityVerifierServer.java
+++ src/IntegrityVerifierServer.java
@@ -1,18 +1,34 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.net.ServerSocketFactory;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class IntegrityVerifierServer {
 
 	private static MACCalculator macCalculator;
 	private ServerSocket serverSocket;
 	private static int seqNumber = 0;
+	private static int IntegrityMessageCount = 0;
+	private static int TotalMessageCount = 0;
+	private static ArrayList<Integer> dailyIntegrity = new ArrayList<Integer>();
+	private static ArrayList<Integer> dailyReceived = new ArrayList<Integer>();
+	static Calendar cal = Calendar.getInstance();
+	static int currentMonth = cal.get(Calendar.MONTH);
+	static int currentDate = cal.get(Calendar.DATE);
 
 	// Constructor del Servidor
 	public IntegrityVerifierServer() throws Exception {
@@ -24,9 +40,23 @@ public class IntegrityVerifierServer {
 
 	// Ejecución del servidor para escuchar peticiones de los clientes
 
-	private void runServer() {
+	private void runServer() throws IOException {
+		PrintWriter pw = new PrintWriter(System.getProperty("user.dir") + "/log.txt");
 		while (true) {
 			// Espera las peticiones del cliente para comprobar mensaje/MAC
+			if (cal.get(Calendar.DATE) != currentDate) {
+				currentDate = cal.get(Calendar.DATE);
+				dailyIntegrity.add(IntegrityMessageCount);
+				IntegrityMessageCount = 0;
+				dailyReceived.add(TotalMessageCount);
+				TotalMessageCount = 0;
+			}
+			if (cal.get(Calendar.MONTH) != currentMonth) {
+				currentMonth = cal.get(Calendar.MONTH);
+				generateMonthlyReport();
+				dailyIntegrity.clear();
+				dailyReceived.clear();
+			}
 			try {
 				System.err.println("Esperando	conexiones	de	clientes...");
 				Socket socket = (Socket) serverSocket.accept();
@@ -45,14 +75,19 @@ public class IntegrityVerifierServer {
 				// System.out.println("message Enviado: "+mensaje);
 				// System.out.println("Own calculated mac: "+macCalculator.calculate(mensaje));
 				// mac del MensajeCalculado
+				TotalMessageCount++;
 				if (seqNum == seqNumber) {
 					if (macMensajeEnviado.equals(macCalculator.calculate(mensaje))) {
 						output.println("Mensaje	enviado	integro	");
+						IntegrityMessageCount++;
 					} else {
 						output.println("Mensaje	enviado	no	integro.");
+						pw.write("Mensaje enviado no integro. Recibido MAC=" + macMensajeEnviado + " pero calculado"
+								+ macCalculator.calculate(mensaje));
 					}
 				} else {
 					output.println("Invalid Sequence Number");
+					pw.write("Numero de secuencia no valido. Esperado: " + seqNumber + " pero recibido: " + seqNum);
 				}
 
 				output.close();
@@ -60,9 +95,26 @@ public class IntegrityVerifierServer {
 				socket.close();
 			} catch (IOException ioException) {
 				ioException.printStackTrace();
+				pw.close();
 			}
 		}
-		
+
+	}
+
+	private void generateMonthlyReport() throws IOException {
+		DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
+		for (int i = 0; i < dailyIntegrity.size(); i++) {
+			line_chart_dataset.addValue(dailyIntegrity.get(i) / dailyReceived.get(i), "Ratio", "" + i);
+		}
+
+		JFreeChart lineChartObject = ChartFactory.createLineChart("Summary ", "Day", "Integrity Ratio",
+				line_chart_dataset, PlotOrientation.VERTICAL, true, true, false);
+
+		int width = 640;
+		int height = 480;
+		File lineChart = new File(System.getProperty("user.dir") + "/report/Monthly Integrity Chart "
+				+ cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.YEAR) + ".jpeg");
+		ChartUtilities.saveChartAsJPEG(lineChart, lineChartObject, width, height);
 
 	}
 
